7783ced70a847fc381c3e1964eba19545896647b
Pratik Narayan <64901855+iampratik1@users.noreply.github.com>
2021-05-15 20:07:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Rosode mein kon tha?",
+    answer: "Rashi ben",
+    options: [
+      "Rashi ben",
+      "Gopi",
+      "Aham Modi",
+      "Kokila modi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Yha hmari  _______ ho rhi?",
+    answer: "Pawri",
+    options: [
+      "Party",
+      "Fighting",
+      "Pawri",
+      "Kutaiii"
+    ]
+  },
+    {
+    numb: 3,
+    question: "agr koi Waqt Badal de ,zazzabat badal de to fir kya krna chahie?",
+    answer: "Usko pakad ke marna chahie",
+    options: [
+      "Sucide",
+      "Oooo Bhaiii..! Maro muzhe",
+      "Usko pakad ke marna chahie",
+      "sanyas le lena chahie"
+    ]
+  },
+    {
+    numb: 4,
+    question: " 2 or 2 kitne hote h?",
+    answer: "22",
+    options: [
+      "4",
+      "22",
+      " 8",
+      "none of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Corona india m kb aya ?",
+    answer: "30 jan 2020",
+    options: [
+      "Roz aata rhta h",
+      "12 nov 2019 ",
+      "22 mar 2020",
+      "30 jan 2020"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
